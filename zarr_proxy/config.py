1c0513df753088eba55c8778020d7eeda337751b
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-02-10 13:53:58 -0700

    add validation for  max chunk size (#14)

---
+++ zarr_proxy/config.py
@@ -0,0 +1,62 @@
+import typing
+
+import pydantic
+
+from .log import get_logger
+
+logger = get_logger()
+
+byte_sizes = {
+    'kb': 1_000,
+    'mb': 1_000**2,
+    'gb': 1_000**3,
+    'kib': 1_024,
+    'mib': 1_024**2,
+    'gib': 1_024**3,
+    'b': 1,
+    'k': 1_000,
+    'm': 1_000**2,
+    'g': 1_000**3,
+    'ki': 1024,
+    'mi': 1_024**2,
+    'gi': 1_024**3,
+}
+
+
+def format_bytes(num: int) -> str:
+    """Format bytes as a human readable string"""
+    return next(
+        (
+            f"{num / value:.2f} {prefix}B"
+            for prefix, value in (
+                ("Gi", 2**30),
+                ("Mi", 2**20),
+                ("ki", 2**10),
+            )
+            if num >= value * 0.9
+        ),
+        f"{num} B",
+    )
+
+
+class Settings(pydantic.BaseSettings):
+    zarr_proxy_payload_size_limit: int = None
+
+    @pydantic.validator('zarr_proxy_payload_size_limit', pre=True)
+    def _validate_zarr_proxy_payload_size_limit(cls, value: typing.Union[int, str]) -> int:
+        if isinstance(value, int):
+            return value
+        if isinstance(value, str):
+            # convert from human readable format to bytes
+            value = value.lower()
+            for key in byte_sizes.keys():
+                if value.endswith(key):
+                    return int(value[: -len(key)]) * byte_sizes[key]
+            raise ValueError(
+                f"Invalid zarr_proxy_payload_size_limit: {value}. Must be an integer or a string with a unit (e.g. '1GB') and valid units are: {', '.join(byte_sizes.keys())}"
+            )
+
+
+def get_settings() -> Settings:
+    logger.info("Loading settings from environment variables")
+    return Settings()
